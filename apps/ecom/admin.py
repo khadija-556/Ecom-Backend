9b83286333a5ce7d5c72dc35f2e4a12187ddbc5b
khadija-556 <khatukhadija556@gmail.com>
2026-05-21 16:21:00 +0600

    Create Product model

--- apps/ecom/admin.py
+++ apps/ecom/admin.py
@@ -8,3 +8,26 @@ class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('first_name','last_name','email','phone','user_status')
     search_fields = ('first_name','last_name','email','phone')
 
+class Product(models.Model):
+    product_code = models.CharField(max_length = 255)
+    title = models.CharField(max_length = 255)
+    ingredients = models.TextField(null=True, blank=True)
+    slug = models.SlugField(max_length=255, null=True, blank=True)
+    PRODUCT_TYPE_CHOICE = (
+        ('single','Single'),
+        ('variable','Variable'),
+    )
+    product_type = models.CharField(max_length=20, choices=PRODUCT_TYPE_CHOICE, default='single')
+    PRODUCT_STATUS_CHOICES = (
+        ('pending','Pending'),
+        ('publish','Publish'),
+    )
+    product_status = models.CharField(max_length=20, choices=PRODUCT_STATUS_CHOICES, default='pending')
+    long_description = models.TextField()
+    short_description = models.TextField()
+    PRODUCT_PRICE_CHOICES = (
+        ('pending','Pending'),
+        ('publish','Publish'),
+    )
+    created_at = models.DateTimeField(auto_now_add = True)
+    update_at = models.DateTimeField(auto_now = True)
